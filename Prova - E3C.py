cfe7a1dbf15d80c04c211b500c08620395aea1fe
Guesser <guesser@outlook.com>
2013-09-25 04:50:26 -0300

    Termino da Prova

--- Prova - E3C.py
+++ Prova - E3C.py
@@ -3,3 +3,22 @@
 #   "fla" quando divisor de 3 
 #   "flu" quando divisor de 5
 #   "flaflu" quando divisor de 3 e 5
+
+contador = 0 # incrementador
+d1 = 3       # Divisor 1
+d2 = 5       # Divisor 2
+m1 = "fla"   # Mensagem referente a MOD Divisor 1
+m2 = "flu"   # Mensagem referente a MOD Divisor 2
+while contador < 100:
+  contador = contador + 1
+  if contador % d1 == 0 and contador % d2 == 0:
+    print(m1+m2)
+  else:
+      if contador % d1 == 0:
+        print(m1)
+      else:
+        if contador % d2 == 0:
+          print(m2)
+        else:
+              print(contador)
+
